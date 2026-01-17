b9f6226f6937bfd3c1b24415b15e128a883bbba6
frontier8004-collab <frontier8004@gmail.com>
2026-01-17 18:31:15 +0900

    v1.2.2.2 tune HOT tile loading (updateWhenIdle/keepBuffer)

--- app.js
+++ app.js
@@ -1270,6 +1270,9 @@ c.setAttribute("tabindex", "0");
 c.focus();
 L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
   maxZoom: 19,
+  updateWhenIdle: true,
+  updateWhenZooming: false,
+  keepBuffer: 2,
   attribution: '&copy; OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team'
 }).addTo(map);
 
