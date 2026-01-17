d7dcd1a770d3793f24fc63242b99d2e268a16787
frontier8004-collab <frontier8004@gmail.com>
2026-01-17 17:59:01 +0900

    v1.2.2.2 revert basemap to CARTO light_all (prepare gray UI)

--- app.js
+++ app.js
@@ -1268,7 +1268,7 @@ applyMovePolicy();
     const c = map.getContainer();
 c.setAttribute("tabindex", "0");
 c.focus();
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   maxZoom: 19,
   attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
 }).addTo(map);
